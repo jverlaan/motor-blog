e972e2896f6ddc1331afe5e10d62a45954735b29
Jan Verlaan <jan@verlaan.org>
2021-12-05 13:41:27 +0100

    add Edit categories functionality

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -14,6 +14,22 @@ class CategoriesController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+      if @category.update(category_params)
+        flash[:notice] = "Category name updated successfully"
+        redirect_to @category
+      else
+        render 'edit'
+      end
+  end
+  
+  
   
   def index
     @categories = Category.paginate(page: params[:page], per_page: 5)
